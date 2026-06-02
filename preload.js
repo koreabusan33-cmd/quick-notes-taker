5ddf9603afbaebb56a1f067382cbba9c235b86fe
koreabusan33-cmd <koreabusan33@gmail.com>
2026-06-02 11:33:36 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,23 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    // Legacy Raw Plaintext Importers
     saveNote: (text) => ipcRenderer.invoke('save-note', text),
     loadNote: () => ipcRenderer.invoke('load-note'),
     saveAs: (text) => ipcRenderer.invoke('save-as', text),
     newNote: () => ipcRenderer.invoke('new-note'),
     openFile: () => ipcRenderer.invoke('open-file'),
     smartSave: (text, filePath) => ipcRenderer.invoke('smart-save', text, filePath),
-    
-    // Header Native Application Frame Menus Forwarding Event Channel Listener
     onMenuAction: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(...args)),
-    
-    // Centralized Structured JSON Database Matrix Channel Invocations
     getNotes: () => ipcRenderer.invoke('get-notes'),
     saveNoteJson: (note) => ipcRenderer.invoke('save-note-json', note),
     deleteNote: (id) => ipcRenderer.invoke('delete-note', id),
-
-    // STEP 4: Add Bridge Methods for Settings (Slide 4)
     getSettings: () => ipcRenderer.invoke('get-settings'),
     saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings)
 });
